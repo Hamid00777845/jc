e06ad0bfff913437875dbcd03e03acbdf73a8ee8
Hamid00777845 <salomatlik87@gmail.com>
2025-06-14 14:23:30 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+let ism = + prompt("Yoshingiz nechida");
+
+console.log(`${ism}- bu malumot turi: ${typeof ism}`);
+
+
+
+let yosh = ! prompt("Yoq uchir ");
+
+console.log(`${yosh} - bu malumot turi ${typeof yosh}`);
+
+
+let sana = ! prompt("Ismingiz nima");
+
+console.log(`${sana} - bu malumot turi ${typeof yosh}`);
+
+
+
